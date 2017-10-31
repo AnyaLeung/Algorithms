739fe9d9233efe0104f3b7ea449a9eaaa5277ac2
anyaliang <anyaliangww@gmail.com>
2017-10-31 09:34:02 +0800

    Update List_Stack_Queue

--- List_Stack_Queue/list.h
+++ List_Stack_Queue/list.h
@@ -3,7 +3,7 @@ struct Node *Ptr_to_node; //PtrToNode --> pointer to node
 struct Ptr_to_node list; 
 struct Ptr_to_node Position;
 
-/* List with header */
+/* List with header L*/
 
 /* function prototype */
 List Make_empty(List L);
@@ -33,6 +33,36 @@ int Is_last(Position P, List L){
     return P->next == NULL;
 }
 
-Position Find(Elem_type P, List L){
-    
+List Init(){
+   List header = malloc(sizeof(Node));
+   return header;
 }
+
+Position Find(Elem_type P, List L){ //find position of ele with value p
+    Ptr_to_node q;
+    q = L->next;
+
+    if(q==NULL) return NULL;
+
+    while(q->next!=P && q){
+        q = q->Next;
+        //if(q->Element==P) return q;
+        //else q = q->Next;
+    }
+    return q; 
+    //provided q is NULL, it stops while NUll, also returns NULL
+}
+
+void Insert(Elem_type X, List L, Position P){
+   //insert Node with value X, after P
+   Position q, tmp;
+
+   q = L->next;
+   while(q->Elem!=P && q){
+       q = q->Next;
+   }
+
+   tmp->Elem = X;
+   tmp->Next = q->Next;
+   q->Next = tmp;
+} 
