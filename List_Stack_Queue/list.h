03482a9c33ab499479746c9ad7dc14ee2d617400
anyaliang <anyaliangww@gmail.com>
2017-11-17 18:20:59 +0800

    Update List

--- List_Stack_Queue/list.h
+++ List_Stack_Queue/list.h
@@ -10,6 +10,7 @@ List Make_empty(List L);
 int Is_empty(List L);
 int Is_last(Position P, List L);
 Position Find(Elem_type X, List L);
+Position Find_previous(Elem_type X, List L);
 void Insert(Elem_type X, List L, Position P);
 void Delete_list(List L);
 Position Header(List L);
@@ -39,38 +40,52 @@ List Init(){
 }
 
 Position Find(Elem_type X, List L){ //find position of ele with value p
-    Ptr_to_node q;
-    q = L->Next;
+    Position p;
 
-    //***
-    while(q!=NULL && q->Element!=X){ 
-        q = q->Next;
-    }
-    return q; 
-    //***
-    //遍历直到找到想要的值/到尾巴也没得
+    p = L->Next;
+    /* *** */
+    while(p!=NULL && p->Element!=X)
+        p = p->Next;
     
+    return p;
+    /* *** */
+    //遍历直到找到想要的值/到尾巴也没得
 } //provided q is NULL, it stops while NUll, also returns NULL
 
+Position Find_previous(Elem_type X, List L){
+    Position P;
+    P = L->Next;
+
+    while(P!=NULL && (P->Next)->Element=X){
+        P = P->Next;
+    }
+    return p;
+}
+
 void Delete(Elem_type X, List L){
-    if(L->Next==NULL) return;
+    if(L->Next==NULL) return; //if list is empty
 
-    Prt_to_no p;
-    p = L->Next;
+    Position p;
+    Node tmp;
 
-    while(p!=NULL && p->Next!=)
+    p = Find_previous(X, L); //刪除要記住前繼node!!
+    if(!Is_last(p, L)){
+        tmp = p->Next; //wanna delete tmp
+        p->Next = tmp->Next; 
+        free(tmp);
+    }
 } //Delete first occurrence of X from a list
 
 void Insert(Elem_type X, List L, Position P){
    //insert Node with value X, after P
-   Position q, tmp;
+   Position tmp;
+   Node ins;
+   ins.Element = X;
 
-   q = L->next;
-   while(q->Elem!=P && q){
-       q = q->Next;
-   }
-
-   tmp->Elem = X;
-   tmp->Next = q->Next;
-   q->Next = tmp;
+   tmp = P->Next;
+   P->Next = ins;
+   ins->Next = tmp;
 } 
+//linked list is linked by pointer
+//so if you give a pointer point to the element
+//it means you can reach that node
