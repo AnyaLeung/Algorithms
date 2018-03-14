b5762c9041ac65e193115b7a8d392441f42b64d6
anyaliang <anyaliangww@gmail.com>
2018-03-14 15:39:36 +0800

    Complete quick_find.cpp in Quick_Find

--- Quick_Find/quick_find.cpp
+++ Quick_Find/quick_find.cpp
@@ -1,15 +1,17 @@
-//要死 寫的是quick find 不是quick union。。
+/*
+ * O(n)
+ */
 #include <iostream>
-//#include <string>
 #include <fstream>
 
 using namespace std;
 
 /* global variable */
 int *id;
+int N = 20;
 
 /* fuction prototype */
-void UF(int N);
+void UF(void);
 int Find(int value1);
 bool Connected(int value1, int value2);
 void Union(int value1, int value2);
@@ -17,7 +19,7 @@ void PrintArr(void);
 /* fuction prototype */
 
 /* fuction definition */
-void UF(int N){
+void UF(void){
     id = new int[N];
     for(int i=0; i<N; i++){
         id[i] = i;
@@ -37,11 +39,11 @@ void Union(int value1, int value2){
     int v2_root = Find(value2);
 
     if(v1_root==v2_root){
-        cout << "is connected";
+        cout << "is connected" << endl;
         return;
     }
     else{
-        for(int i=0; i<20; i++){
+        for(int i=0; i<N; i++){
             if(id[i]==v1_root)
                 id[i] = v2_root;
         }
@@ -51,58 +53,24 @@ void Union(int value1, int value2){
 
 void PrintArr(void){
     cout << "value  ";
-    for(int i=0; i<20; i++){
+    for(int i=0; i<N; i++){
         cout << i << " " ;
     }
     cout << endl;
 
     cout << "father ";
-    for(int i=0; i<20; i++){
+    for(int i=0; i<N; i++){
         cout << id[i] << " ";
     }
     cout << endl << endl;
 }
 
 //prototype
-string GetFileName(){
-    int choice = 0;
-
-    cout << "Enter [1] to load case1_input," << endl;
-    cout << "Enter [2] to load case2_input: " << endl;
-    cin >> choice;
-
-    if(choice==1)
-        return "case1_input.txt";
-    if(choice==2){
-        return  "case2_input.txt";
-    }
-}
-
-
-void InputFileProcess(void){
-    int N = 0; // id[] length
-    int tmp1 = 0, tmp2 = 0;
-    string name;
-    name = GetFileName();
-    ifstream = infile(name);
-
-    infile >> tmp1;
-    infile >> tmp2;
-
-    N = (tmp1>tmp2)? tmp1 : tmp2;
-    //按理來說到這裡已經讀了前兩行 知道了id[]的length
-    
-    while(!infile.eof()){
-        infile >> tmp1 >> tmp2;
-        cout << tmp1 << tmp2;
-    }
-} //我他媽不會寫file啊真是要跪了
-//後幾行要把兩個參數的讀進去 然後union
-//還需要把結果存到file裡 怕了怕了 我去看computer orginization了。。
-
 /* fuction definition */
 
 int main(void){
-    UF(N);
+    UF();
+    Union(1, 2);
+    Union(0, 1);
     return 0;
 }
